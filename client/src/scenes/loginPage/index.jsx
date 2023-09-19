eb3c12b5aecce00c539e607bac3ee591a14adf91
pradysriv02 <116811317+pradysriv02@users.noreply.github.com>
2023-09-19 13:10:46 +0530

    Update 2.0

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,57 +1,54 @@
 import React from "react";
-import { Box,Typography,useTheme,useMediaQuery } from "@mui/material";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import Form from "./Form";
 
-
-
 const LoginPage = () => {
-    const theme = useTheme();
-    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-    return (
-      <Box>
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center">
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          SocialApp
+        </Typography>
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap="4rem"
+        position="relative">
         <Box
-          width="100%"
-          backgroundColor={theme.palette.background.alt}
-          p="1rem 6%"
-          textAlign="center"
-        >
-          <Typography fontWeight="bold" fontSize="32px" color="primary">
-            SocialApp
-          </Typography>
-        </Box>
-        <Box display="flex" alignItems="center" justifyContent="center" gap="4rem" position="relative">
-          
-          <Box
           width="60%"
           p="2rem"
           m="2rem"
           borderRadius="1.5rem"
-          backgroundColor={theme.palette.background.alt}
-        >
+          backgroundColor={theme.palette.background.alt}>
           <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-            Welcome to SocialApp,the next evolution in the world of social media!!!
+            Welcome to SocialApp,the next evolution in the world of social
+            media!!!
           </Typography>
           <Form />
         </Box>
-          </Box>
-          <Box
-          width={isNonMobileScreens ? "30%" : "93%"}
-          p="2rem"
-          m="2rem "
-          borderRadius="1.5rem"
-          backgroundColor={theme.palette.background.alt}
-        >
-          <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-            Welcome to SocialApp,the next evolution in the world of social media!!!
-          </Typography>
-          <Form />
-        </Box>
-        </Box>
-        
-        
-        
       </Box>
-    );
-  };
-  
-  export default LoginPage;
+      <Box
+        width={isNonMobileScreens ? "30%" : "93%"}
+        p="2rem"
+        m="2rem "
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}>
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to SocialApp,the next evolution in the world of social
+          media!!!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
